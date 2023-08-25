36f9d8105286d7cee37ff329e6dee975edc56d64
Alan Wolfe <awolfe@ea.com>
2023-08-25 10:16:09 -0700

    It works, but can't have slang load from memory

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "slang/slang-gfx.h"
+#include <vector>
 
 using namespace Slang;
 using namespace slang;
@@ -11,8 +12,75 @@ static const char*              c_entryPointName        = "csmain";
 static const SlangCompileTarget c_compileTarget         = SlangCompileTarget::SLANG_HLSL;
 static const char*              c_compileProfile        = "cs_5_1";
 
+// Needed to store the source code in
+class Blob : public ISlangBlob
+{
+public:
+    Blob(void* data, size_t size)
+        : m_data(data)
+        , m_size(size)
+    {
+    }
+
+    virtual SLANG_NO_THROW void const* SLANG_MCALL getBufferPointer()
+    {
+        return m_data;
+    }
+
+    virtual SLANG_NO_THROW size_t SLANG_MCALL getBufferSize()
+    {
+        return m_size;
+    }
+
+    virtual SLANG_NO_THROW uint32_t SLANG_MCALL addRef()
+    {
+        return 1;
+    }
+
+    virtual SLANG_NO_THROW uint32_t SLANG_MCALL release()
+    {
+        return 1;
+    }
+
+    SlangResult queryInterface(SlangUUID const& guid, void** outObject)
+    {
+        if (auto intf = getInterface(guid))
+        {
+            *outObject = intf;
+            return SLANG_OK;
+        }
+        return SLANG_E_NO_INTERFACE;
+    }
+
+    void* castAs(const SlangUUID& guid)
+    {
+        if (auto intf = getInterface(guid))
+        {
+            return intf;
+        }
+        return getObject(guid);
+    }
+
+    ISlangUnknown* getInterface(const Guid& guid)
+    {
+        return static_cast<ISlangBlob*>(this);
+    }
+
+    void* getObject(const Guid& guid)
+    {
+        SLANG_UNUSED(guid);
+        return nullptr;
+    }
+
+protected:
+    void* m_data = nullptr;
+    size_t m_size = 0;
+};
+
 int main(int argc, char** argv)
 {
+
+
 	// Create the global session. This is costly so should only be done once per application launch if possible.
 	ComPtr<IGlobalSession> globalSession;
 	createGlobalSession(globalSession.writeRef());
@@ -27,9 +95,29 @@ int main(int argc, char** argv)
 	sessionDesc.targetCount = 1;
 	globalSession->createSession(sessionDesc, session.writeRef());
 
-    // Load the source file
     ComPtr<IBlob> diagnosticsBlob;
+
+    // The first block has slang load the source from disk.
+    // The second block (in the else) has slang load the source from memory, but is crashing for some reason.
+#if 1
     IModule* module = session->loadModule(c_fileNameSource, diagnosticsBlob.writeRef());
+#else
+    std::vector<char> source;
+    {
+        FILE* file = nullptr;
+        fopen_s(&file, c_fileNameSource, "rb");
+        fseek(file, 0, SEEK_END);
+        source.resize(ftell(file) + 1, 0); // an extra byte for a null terminator
+        fseek(file, 0, SEEK_SET);
+        fread(source.data(), 1, source.size() - 1, file);
+        fclose(file);
+    }
+    Blob sourceBlob(source.data(), source.size() - 1);
+    ComPtr<Blob> sourceBlobPtr(&sourceBlob);
+
+    IModule* module = session->loadModuleFromSource(c_fileNameSource, c_fileNameSource, sourceBlobPtr, diagnosticsBlob.writeRef());
+#endif
+
     if (diagnosticsBlob)
         printf("%s", (const char*)diagnosticsBlob->getBufferPointer());
     if (!module)
@@ -113,5 +201,3 @@ int main(int argc, char** argv)
 
 	return 0;
 }
-
-// TODO: load this from memory before calling it G2G
